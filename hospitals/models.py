701eb3199797a583e26a0d24a2a11ab5c67006b5
Alan Fachini <alfakini@gmail.com>
2020-04-21 17:28:34 -0300

    Add HospitalBed and PatientBed models

--- hospitals/models.py
+++ hospitals/models.py
@@ -18,11 +18,26 @@ class DepartureTypes(models.TextChoices):
     O = "O", "Óbito"
 
 
+class BedTypes(models.TextChoices):
+    UTIA = "UTIA", "UTI Adulto"
+    UTIP = "UTIP", "UTI Pediátrica"
+    UTIN = "UTIN", "UTI Neonatal"
+    CA = "CA", "Clínico Adulto"
+    CP = "CP", "Clínico Pediátrico"
+    CN = "CN", "Clínico Neonatal"
+    VM = "VM", "Leito Símples com Ventilador Mecânico"
+
+
 class Hospital(models.Model):
     name = models.CharField("Nome", blank=False, max_length=254)
     city = models.CharField("Cidade", blank=False, max_length=254)
     phonenumber = models.CharField("Telefone", blank=False, max_length=16)
     email = models.EmailField("E-mail", blank=False, max_length=254)
+
+
+class HospitalBed(models.Model):
+    hospital = models.ForeignKey(Hospital, verbose_name="Hospital", on_delete=models.CASCADE)
+    beds = models.CharField("Tipo do Leito", blank=False, choices=BedTypes.choices, default=BedTypes.UTIA, max_length=38)
 
 
 class Patient(models.Model):
@@ -39,3 +54,10 @@ class Patient(models.Model):
     cns = models.CharField("Carteira Nacional do SUS", blank=True, default="", max_length=30)
     sisreg = models.CharField("Número no sistema Sisreg", blank=True, default="", max_length=30)
     departure_reason = models.CharField("Motivo da Saída", blank=True, choices=DepartureTypes.choices, default=DepartureTypes.A, max_length=5)
+
+
+class PatientBed(models.Model):
+    patient = models.ForeignKey(Patient, verbose_name="Paciente", on_delete=models.CASCADE)
+    bed = models.CharField("Tipo do Leito", blank=False, choices=BedTypes.choices, default=BedTypes.UTIA, max_length=38)
+    admission_date = models.DateField("Data de Admissão neste Leito", blank=False, auto_now=False, auto_now_add=False)
+    waiting_uti = models.BooleanField("Aguardando UTI", blank=False, default=False)
