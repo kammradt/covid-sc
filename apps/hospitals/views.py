a85d23b1970c471793f3d1866eb200131fee5356
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-25 15:38:09 -0300

    refactor(hospital): fixing to only show values
    Co-authored-by: Henrique de Castilhos <henriquedecastilhos@gmail.com>

--- apps/hospitals/views.py
+++ apps/hospitals/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
-from apps.hospitals.models import Hospital
+from apps.hospitals.models import Hospital, Report
 
 
 def index(request):
     return render(request, 'index.html', {})
 
 
-def hospital_general_data(request, hospital_id):
+def show(request, hospital_id):
     hospital = Hospital.objects.get(pk=hospital_id)
+    report = Report.objects.filter(hospital=hospital_id).order_by('-report_date').first()
 
-    return render(request, 'hospital/general_data.html', {
-        'hospital': hospital
+    return render(request, 'hospital/show.html', {
+        'hospital': hospital,
+        'report': report
     })
